a4d5f9982f7476dd06a425f49ad2f6750ba1db3f
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-03 00:47:29 +0500

    Update [navbarProducts].jsx

--- pages/products/[navbarProducts].jsx
+++ pages/products/[navbarProducts].jsx
@@ -1,22 +1,54 @@
 import { getNav, getNavProducts } from "@/services/products";
 import MyHeaderFooter from "@/Components/MyHeaderFooter";
+import Link from "next/link";
 import React from "react";
-import { Descriptions } from "antd";
+import { Card } from "antd";
+const { Meta } = Card;
 
-export default function App({ data, navNames }) {
+export default function App({ allProducts, navNames }) {
+  if (!allProducts) {
+    return <div>Loading...</div>;
+  }
+  const productList = allProducts.map((product) => {
+    return (
+      <>
+        <Link href={`/${product[""]}`}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+              margin: "5px",
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://th.bing.com/th/id/R.2b2ac1bb5b369a4aea0e5dc1768f10b4?rik=uXHEZ0EPPT7U7w&pid=ImgRaw&r=0"
+              />
+            }
+          >
+            <Meta
+              title={`${product.Name}`}
+              description={`Rs ${product.Price}`}
+            />
+          </Card>
+        </Link>
+        <br />
+      </>
+    );
+  });
   return (
     <>
       <MyHeaderFooter nav={navNames}>
-        <div style={{ width: "800px", margin: "0 auto" }}>
-          <Descriptions bordered column={1}>
-            {Object.entries(data)
-              .slice(1)
-              .map(([key, value]) => (
-                <Descriptions.Item key={key} label={key}>
-                  {value}
-                </Descriptions.Item>
-              ))}
-          </Descriptions>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "space-between",
+          }}
+        >
+          {productList}
         </div>
       </MyHeaderFooter>
     </>
@@ -27,9 +59,10 @@ export async function getServerSideProps(context) {
   const { params, req, res } = context;
   const navNames = getNav();
   const data = getNavProducts(params.navbarProducts);
+  console.log(data);
   return {
     props: {
-      data,
+      allProducts: data,
       navNames: navNames,
     },
   };
