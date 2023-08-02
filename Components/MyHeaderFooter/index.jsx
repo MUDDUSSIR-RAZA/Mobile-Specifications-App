9712a0e49acd2894f1c085d8944f9bff93e39233
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-08-02 18:01:13 +0500

    Update index.jsx

--- Components/MyHeaderFooter/index.jsx
+++ Components/MyHeaderFooter/index.jsx
@@ -75,24 +75,6 @@ const App = ({ children }) => {
           items={items}
         />
       </Sider>
-      {/* <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={items}
-        />
-      </Sider> */}
       <Layout
         className="site-layout"
         style={{
@@ -113,7 +95,6 @@ const App = ({ children }) => {
         </Header>
         <Content
           style={{
-            border: "red solid",
             margin: "70px 0px 50px 12px",
             overflow: "initial",
             textAlign: "center",
@@ -151,72 +132,3 @@ const App = ({ children }) => {
   );
 };
 export default App;
-
-// import React from "react";
-
-// import {
-//   UploadOutlined,
-//   UserOutlined,
-//   VideoCameraOutlined,
-// } from "@ant-design/icons";
-// import { Layout, Menu, theme } from "antd";
-// const { Header, Content, Footer, Sider } = Layout;
-// const App = ({ children }) => {
-//   const {
-//     token: { colorBgContainer },
-//   } = theme.useToken();
-//   return (
-//     <Layout>
-//       <Sider
-//         breakpoint="lg"
-//         collapsedWidth="0"
-//         onBreakpoint={(broken) => {
-//           console.log(broken);
-//         }}
-//         onCollapse={(collapsed, type) => {
-//           console.log(collapsed, type);
-//         }}
-//       >
-//         <div className="demo-logo-vertical" />
-//         <Menu
-//           theme="dark"
-//           mode="inline"
-//           defaultSelectedKeys={["4"]}
-//           items={[
-//             UserOutlined,
-//             VideoCameraOutlined,
-//             UploadOutlined,
-//             UserOutlined,
-//           ].map((icon, index) => ({
-//             key: String(index + 1),
-//             icon: React.createElement(icon),
-//             label: `nav ${index + 1}`,
-//           }))}
-//         />
-//       </Sider>
-//       <Layout>
-//         <Header
-//           style={{
-//             padding: 0,
-//             background: colorBgContainer,
-//           }}
-//         />
-//         <Content
-//           style={{
-//             margin: "24px 16px 0",
-//           }}
-//         >
-//           {children}
-//         </Content>
-//         <Footer
-//           style={{
-//             textAlign: "center",
-//           }}
-//         >
-//           Ant Design ©2023 Created by Ant UED
-//         </Footer>
-//       </Layout>
-//     </Layout>
-//   );
-// };
-// export default App;
